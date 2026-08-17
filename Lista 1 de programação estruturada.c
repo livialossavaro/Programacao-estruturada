2524e6a5564ba1d9eea1fe8bef7c1edf237ddf76
Lívia Lossavaro Arias <livialossavaro7@gmail.com>
2026-08-17 19:10:56 -0300

    Add files via upload

---
+++ Lista 1 de programação estruturada.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+
+	/*int soma(int a, int b);
+
+	int main () {
+	
+	int a = 4, b = 6, r;
+	r = soma(a, b);
+	
+	printf("Soma: %d", r);
+	
+}
+
+	int soma(int a, int b) {
+		return a + b;	
+		}*/
+		
+	/*int par(int n);
+	
+	int main () {
+	
+		int a = 2;
+		printf("%d", par(a));
+		
+		return 0;
+	}
+	
+	int par(int n) {
+		if (n % 2 == 0) {
+			return 1;
+		}
+		return 0;
+	}*/
+	
+	/*int maior(int a, int b);
+	
+	int main () { 
+	
+	int a = 2, b = 5;
+	
+	printf("Maior: %d", maior(a,b));
+	
+	return 0;
+	}
+	
+	int maior(int a, int b) {
+		if (a > b) {
+			return a;
+		}
+		return b;
+	}*/
+	
+	/*int potencia(int base, int expoente);
+	
+	int main () {
+			
+	printf("Potencia: %d", potencia(3,2));
+		
+	return 0;
+	}
+
+	int potencia(int base, int expoente) {
+		
+		int i, result = 1;
+		
+		for (i = 0; i < expoente; i++) {
+			result *= base;
+		}
+		return result;
+	}*/
+	
+	/*int fatorial(int n);
+	
+	int main () {
+		
+	int a = 5;
+	
+	printf("Fatorial: %d", fatorial(a));
+	}
+	
+	int fatorial(int n) {
+		int i, result = 1;
+		for (i = n; i > 0; i--) {
+			result *= i;
+		}
+		return result;
+	}*/
+	
+	/*int fatorial_rec(int n);
+	
+	int main () {
+		int a = 5;
+		printf("Fatorial: %d", fatorial_rec(a));
+	}
+	
+	int fatorial_rec(int n) {
+	
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	
+	return n * fatorial_rec(n-1);
+	}*/
